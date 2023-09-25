74c1ac81ef6e6b20f32194286fded84a5f8b60f6
Emine Çetin <80969567+eminecetin@users.noreply.github.com>
2023-09-25 17:55:41 +0300

    Emine ile giriş yazısı değiştirme

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Archis Academy ile github eğitimi..
+          Git & Github Öğreniyorum w/emine
         </p>
         <a
           className="App-link"
